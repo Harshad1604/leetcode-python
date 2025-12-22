0c312693a123fd504705ffa5cdbf231e97659d78
Harshad1604 <147176921+Harshad1604@users.noreply.github.com>
2025-12-23 00:28:54 +0530

    Remove misplaced root-level Leetcode files

--- lc_1523_count_odd_numbers.py
+++
@@ -1,7 +0,0 @@
-# LeetCode 1523: Count odd numbers in an interval
-
-class Solution:
-    def countOdds(self, low: int, high: int) -> int:
-        count = (high+1)//2 - (low//2)
-
-        return count
